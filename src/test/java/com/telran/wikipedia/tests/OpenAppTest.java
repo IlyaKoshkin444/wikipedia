c72e084651350ae8dc3772f103a2071a8b12ae6d
Marina <stritenko@gmail.com>
2020-03-01 16:44:23 +0200

    created 2 level project wikipedia add class SearchArticleTest

---
+++ src/test/java/com/telran/wikipedia/tests/OpenAppTest.java
@@ -0,0 +1,14 @@
+package com.telran.wikipedia.tests;
+
+import com.telran.wikipedia.tests.TestBase;
+import org.testng.annotations.Test;
+
+public class OpenAppTest extends TestBase {
+
+    @Test
+    public void testOpenApp() {
+        System.out.println("App opened");
+
+    }
+
+}
